9e96de87cfe7bf2dd83931063c94a3dbf57a63a0
Tanya <tanyachauhan0076@gmail.com>
2026-01-12 14:59:40 +0530

    chapter3

---
+++ str_functions_3.py
@@ -0,0 +1,14 @@
+name = "Tanya"
+
+print(len(name))
+print(name.endswith("nya"))
+print(name.startswith("Ta"))
+print(name.capitalize())
+print(name.lower())
+print(name.upper())
+print(name.find("an"))
+
+s = "hello world"
+print(s.find("world"))
+print(s.replace("world", "Tanya"))
+print(s.title())
